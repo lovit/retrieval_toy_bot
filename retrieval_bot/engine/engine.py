62ebc32002e1f73ee59de5375332bbda5a24959e
lovit <soy.lovit@gmail.com>
2018-04-19 05:28:06 +0900

    Apply evaluator

--- retrieval_bot/engine/engine.py
+++ retrieval_bot/engine/engine.py
@@ -1,19 +1,23 @@
+import scipy
 from .utils import DefaultMessage
 
 class InstanceBasedRetrievalEngine:
 
-    def __init__(self, vectorizer, send2reply, send_indexer,
-                 scoring=None, preprocessor=None, default_message=None):
+    def __init__(self, vectorizer, send2reply, send_indexer, reply_x,
+                 evaluator=None, preprocessor=None, default_message=None):
 
         self.preprocessor = preprocessor
         self.vectorizer = vectorizer
         self.send2reply = send2reply
         self.send_indexer = send_indexer
-        self.scoring = scoring
+        self.reply_x = reply_x
+        if type(reply_x) != scipy.sparse.csr.csr_matrix:
+            self.reply_x = self.repyl_x.tocsr()
+        self.evaluator = evaluator
         self.default_message = default_message if default_message else DefaultMessage()
 
     def process(self, send, n_reply=5, n_similar_sends=5,
-                max_send_distance=0.5):
+                max_send_distance=0.5, debug=False):
 
         # Preprocessing
         if self.preprocessor:
@@ -25,32 +29,46 @@ class InstanceBasedRetrievalEngine:
             return self.default_message.get_random_message()
 
         # Get similar sends
-        send_dist, send_idxs = self.send_indexer.kneighbors(
+        similar_dist, similar_idxs = self.send_indexer.kneighbors(
             send_vector, n_similar_sends, max_send_distance)
-        if send_idxs.shape[0] == 0:
+        if similar_idxs.shape[0] == 0:
             return self.default_message.get_random_message()
 
         # Get reply candidates
         candidate_text = {}
         candidate_count = {}
-        for send_idx in send_idxs:
+        for similar_idx in similar_idxs:
             # format [(idx, text, count), ... ]
-            replies = self.send2reply.get_replies(send_idx)
-            for idx, text, count in replies:
-                candidate_text[idx] = text
-                candidate_count[idx] = count
+            replies = self.send2reply.get_replies(similar_idx)
+            for reply_idx, text, count in replies:
+                candidate_text[reply_idx] = text
+                candidate_count[reply_idx] = count
 
-        # Scoring
+        # Return default message if exists no candidates
         if not candidate_count:
             return self.default_message.get_random_message()
 
-        # Sorting
-        # format {idx:text, ... }
-        replies = sorted(candidate_text.items(), 
-                         key=lambda x:-candidate_count[x[0]])
+        # Evaluating candidates
+        if not self.evaluator:
+            # sort by frequency
+            # format {idx:text, ... }
+            replies = sorted(candidate_text.items(),
+                             key=lambda x:-candidate_count[x[0]])
+            replies = [(reply_idx, text, candidate_count[reply_idx])
+                       for reply_idx, text in replies]
+        else:
+            scores = []
+            send_terms = send_vector.nonzero()[1]
+            for reply_idx in candidate_text:
+                reply_terms = self.reply_x[reply_idx,:].nonzero()[1]
+                scores.append((reply_idx, self.evaluator.evaluate(send_terms, reply_terms)))
+            replies = sorted(scores, key=lambda x:-x[1])
+            replies = [(reply_idx, candidate_text[reply_idx], score)
+                       for reply_idx, score in replies]
 
         if n_reply > 0:
             replies = replies[:n_reply]
 
-        return [text for idx, text in replies]
-        
+        if debug:
+            return replies
+        return [text for reply_idx, text, score in replies]
